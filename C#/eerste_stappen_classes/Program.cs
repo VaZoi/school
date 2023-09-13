eee186491296e2f96a05c0ed0695a374abe53716
VZ <131747053+VaZoi@users.noreply.github.com>
2023-09-13 15:49:00 +0200

    C#

--- C#/eerste_stappen_classes/Program.cs
+++ C#/eerste_stappen_classes/Program.cs
@@ -2,9 +2,9 @@
 {
     static void Main()
     {
-        Student student1 = new Student("Julie", 2006, 2169020);
-        Student student2 = new Student("Lucas", 2005, 2154727);
-        Student student3 = new Student("Martino", 2007, 2167346);
+        Student student1 = new Student("Julie", 2006, 2169020, 8.7F);
+        Student student2 = new Student("Lucas", 2005, 2154727, 7.9F);
+        Student student3 = new Student("Martino", 2007, 2167346, 8.5F);
 
         student1.Print();
         student2.Print();
--- C#/eerste_stappen_classes/student.cs
+++ C#/eerste_stappen_classes/student.cs
@@ -13,14 +13,20 @@ class Student
         get; set; 
     }
 
-    public Student(string naam, int geboortejaar, int studentennummer)
+    public float Gemiddeld_Cijfer
+    {
+        get; set;
+    }
+
+    public Student(string naam, int geboortejaar, int studentennummer, float gemiddeld_cijfer)
     {
         Naam = naam;
         Geboortejaar = geboortejaar;
         Studentennummer = studentennummer;
+        Gemiddeld_Cijfer = gemiddeld_cijfer;
     }
     public void Print()
     {
-        Console.WriteLine($"Naam: {Naam}, Geboortejaar: {Geboortejaar}, Studentennummer: {Studentennummer}");
+        Console.WriteLine($"Naam: {Naam}, Geboortejaar: {Geboortejaar}, Studentennummer: {Studentennummer}, Gemiddelde Cijfer: {Gemiddeld_Cijfer}");
     }
 }
