5f6bc6862d435b532db8575c6d7442edabd95ed9
VZ <131747053+VaZoi@users.noreply.github.com>
2023-10-19 09:35:02 +0200

    7

--- Laravel/opdracht_eerste_route/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-Route::get('/planeten', function () {
-    $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return response()->json($planeten);
-});
-
-?>
