3a5e42018c68e612031e6de07e25f2f791598c44
VZ <131747053+VaZoi@users.noreply.github.com>
2023-11-13 12:31:31 +0100

    s

--- PDO/02-YEAR/02-PERIODE/02-LES/db.php
+++ PDO/02-YEAR/02-PERIODE/02-LES/db.php
@@ -13,16 +13,32 @@ class database {
         $this->host = 'localhost';
         $this->username = 'root';
         $this->password = '';
-        $this->database = 'school';
+        $this->database = 'test';
         $this->port = 3307;
     
         $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->database";
         try {
             $this->dbh = new PDO($dsn, $this->username, $this->password);
             $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connection established";
+            echo "Connection established" . PHP_EOL;
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
+        }
+    }
+    public function insert($product_naam, $product_beschrijving) {
+        $sql = "INSERT INTO producten (naam, beschrijving) VALUES (:naam, :beschrijving)";
+        
+        try {
+            $stmt = $this->dbh->prepare($sql);
+
+            $stmt->bindParam(':naam', $product_naam);
+            $stmt->bindParam(':beschrijving', $product_beschrijving);
+
+            $stmt->execute();
+
+            echo "Data successfully inserted into the database" . PHP_EOL;
+        } catch (PDOException $e) {
+            die("Insert failed: " . $e->getMessage());
         }
     }
 
--- PDO/02-YEAR/02-PERIODE/02-LES/home.php
+++ PDO/02-YEAR/02-PERIODE/02-LES/home.php
@@ -3,4 +3,9 @@
 include("db.php");
 $db = new Database;
 
+$product_naam = "Kauwgom";
+$product_beschrijving = "6 stuks, meloensmaak";
+
+$db->insert($product_naam, $product_beschrijving);
+
 ?>
