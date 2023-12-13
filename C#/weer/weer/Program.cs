710ea2dcc89d703050893eb0d455eb29fd10b625
VZ <131747053+VaZoi@users.noreply.github.com>
2023-12-13 16:24:58 +0100

    l

--- C#/weer/weer/Program.cs
+++ C#/weer/weer/Program.cs
@@ -2,6 +2,13 @@
 using WeerData;
 
 var weather = new Weerdata();
+weather.ErrorOccurred += Weather_ErrorOccurred;
+
+void Weather_ErrorOccurred(object? sender, WeatherEventArgs e)
+{
+    Console.WriteLine($"Error: {e.Line}");
+}
+
 weather.ReadFile();
 
 
--- C#/weer/weer/obj/Debug/net8.0/weer.AssemblyInfo.cs
+++ C#/weer/weer/obj/Debug/net8.0/weer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8f3c973efa7cfed07aa6c8db7c94c5e35d06d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911ef46044ffb5ab8ab5f83a4d1ec2c688fc44c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("weer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
