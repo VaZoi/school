d7d6754b3894b96833ee09ac2e3990a5af93e4c2
VZ <131747053+VaZoi@users.noreply.github.com>
2023-12-14 09:42:44 +0100

    added copy of console program

--- C#/weer/weer/obj/Debug/net8.0/weer.AssemblyInfo.cs
+++ C#/weer/weer/obj/Debug/net8.0/weer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911ef46044ffb5ab8ab5f83a4d1ec2c688fc44c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("weer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
