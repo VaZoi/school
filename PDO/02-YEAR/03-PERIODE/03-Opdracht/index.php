948d7d54711da57d9725983fea889869ba18056c
VZ <131747053+VaZoi@users.noreply.github.com>
2024-02-12 12:00:09 +0100

    Update index.php

--- PDO/02-YEAR/03-PERIODE/03-Opdracht/index.php
+++ PDO/02-YEAR/03-PERIODE/03-Opdracht/index.php
@@ -1,2 +1,43 @@
 <?php
 
+class DB 
+{
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    private $port;
+    private $dbh;
+    
+
+    public function __construct() 
+    {
+        $this->host = 'localhost';
+        $this->username = 'root';
+        $this->password = '';
+        $this->database = 'recipesforyou';
+        $this->port = 3306;
+
+        $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->database";
+        try {
+            $this->dbh = new PDO($dsn, $this->username, $this->password);
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection error: " . $e->getMessage());
+        }
+    }
+
+    // function to make sql query easy and not write the same code every time
+    public function run($query, $args = null): PDOStatement | false
+    {
+        try {
+            $stmt = $this->dbh->prepare($query);
+            $stmt->execute($args);
+            return $stmt;
+        } catch (PDOException $e) {
+            die("Query error: " . $e->getMessage());
+        }
+    }
+}
+
